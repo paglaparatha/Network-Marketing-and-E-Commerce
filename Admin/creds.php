045697af9991e7adc7a4f027aef2eb53acff6ef2
paglaparatha <31624695+paglaparatha@users.noreply.github.com>
2020-10-03 02:47:48 +0530

    Update creds.php

--- Admin/creds.php
+++ Admin/creds.php
@@ -1,6 +1,6 @@
 <?php 
 
 date_default_timezone_set('Asia/Kolkata');
-$con = new mysqli('localhost', 'beardcod_barney', 'Pata nahi', 'beardcod_networkx');
+$con = new mysqli('localhost', 'root', '', 'networkx');
 
 ?>
